b8cbd28a737f563bd9c02f73c30103398e210e79
Mahesh Chigurupati <mchigurupati92@gmail.com>
2023-11-15 17:37:30 -0800

    Added post, put and patch cateringJobs apis and actuator healthchecks along with spring scheduler

--- src/main/java/com/cecilireid/springchallenges/CateringJobRepository.java
+++ src/main/java/com/cecilireid/springchallenges/CateringJobRepository.java
@@ -1,11 +1,12 @@
 package com.cecilireid.springchallenges;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface CateringJobRepository extends JpaRepository<CateringJob, Long> {
+public interface CateringJobRepository extends CrudRepository<CateringJob, Long> {
 
     List<CateringJob> findByStatus(Status status);
 }
